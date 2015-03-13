fda1567dea53b6ee3d8701e0b607e05619902dbe
Brittany Robertson <brittanyrob@live.ca>
2015-03-13 09:59:56 -0400

    .

--- send-mail.php
+++ send-mail.php
@@ -14,7 +14,7 @@ $frm_message = stripcslashes($_POST['message']);
 Variables you can change
 ============================================== */
 
-$mailto = 'info@spectrapainters.ca'; // Enter your mail addres here. 
+$mailto = 'brittanyrob@live.ca'; // Enter your mail addres here. 
 $subject = $frm_subject; // Enter the subject here.
 
 $error_message = 'Error sending your message'; // Message displayed if an error occurs
